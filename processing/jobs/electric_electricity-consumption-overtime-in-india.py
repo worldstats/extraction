38a15629df9c748b06994058f6d5030599a56fc9
Krishna Sagar R <krishnasagar@Macs-MacBook-Pro.local>
2021-10-03 19:21:40 +0100

    add some more data

--- processing/jobs/electric_electricity-consumption-overtime-in-india.py
+++ processing/jobs/electric_electricity-consumption-overtime-in-india.py
@@ -4,28 +4,55 @@ import json
 from utils.execute_current import execute_current
 
 raw_file = "./raw/electricity-generation.csv"
+raw_file2 = "./raw/energy-consumption-by-source-and-region.csv"
 chart_file = "./charts/electric_india-electricity-consumption-over-time.json"
 
-def toChart():
 
-    s = pd.read_csv(raw_file)
-    filtered = s[s["Entity"] == "India"]
-    points = []
-    for index in filtered.index:
-        points.append({
-            "x": filtered["Year"][index],
-            "y": int(filtered["Electricity Generation (TWh)"][index])
+def toChart():
+    totals = pd.read_csv(raw_file)
+    cres = pd.read_csv(raw_file2)
+    fossil_fuels = []
+    green_energy = []
+
+    cres_india = cres[cres["Entity"] == "India"]
+
+    for index in cres_india.index:
+        fossil_fuels.append({
+            "x": cres_india["Year"][index],
+            "y": int(cres_india["Coal Consumption - EJ"][index])
+                 + int(cres_india["Oil Consumption - EJ"][index])
+                 + int(cres_india["Gas Consumption - EJ"][index])
+                 + int(cres_india["Nuclear Consumption - EJ"][index])
+        })
+
+    for index in cres[cres["Entity"] == "India"].index:
+        green_energy.append({
+            "x": cres_india["Year"][index],
+            "y": int(cres_india["Hydro Consumption - EJ"][index])
+                 + int(cres_india["Wind Consumption - EJ"][index])
+                 + int(cres_india["Geo Biomass Other - EJ"][index])
+                 + int(cres_india["Biofuels (TWh)"][index])
+                 + int(cres_india["Solar Consumption - EJ"][index])
         })
 
     return {
         "type": "TimeSeries",
         "series": [{
-            "color": "pink",
+            "color": "brown",
             "type": "bar",
             "opacity": 0.5,
             "name": "Fossil Fuels",
-            "data": points
-        }]
+            "data": fossil_fuels
+        },
+            {
+                "color": "green",
+                "type": "bar",
+                "opacity": 1,
+                "name": "Renewable Energy",
+                "data": green_energy
+            }
+        ]
     }
+
 
 execute_current()
